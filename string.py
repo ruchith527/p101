337e2af2830ae46a7301962ad0211382510deab8
ruchith527 <82458071+ruchith527@users.noreply.github.com>
2022-01-03 15:30:04 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,6 @@
+#sting object
+myIntro = 'My name is Ruchith, I am 15 years old'
+words = myIntro.split()
+print(words)
+
+
---
+++ userdefinedfunction.py
@@ -0,0 +1,15 @@
+#def functionName():
+    #<block of code>
+
+#def functionName(variable):
+    #<block of code>
+
+#function definition
+def greet(name):
+    print("Hello, " + name + ". How are you?")
+
+#function call
+
+greet('Ruchith')
+greet('Saloni')
+greet('Ratan')
